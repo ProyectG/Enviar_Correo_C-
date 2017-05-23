4c5994705ccf84e2e209d92dafe98f4484bf12bc
proyectg <marco.saffie@gmail.com>
2017-05-22 22:48:34 -0400

    Agregar archivos de proyecto.

---
+++ ConsoleApplication2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            confmail conf = new confmail(); 
+           try
+            {
+                MailMessage config = new MailMessage(); 
+                config = conf.definir_correo("correo1@correo.com", "correo2@correo.com","Test","<h1>Exito</h1>");
+                SmtpClient servidor = new SmtpClient();
+                servidor = conf.smtpConfig(2252, "gmail.com", "tucorreo@gmail.com", "password");
+
+                //Intentar enviar correo
+
+                servidor.Send(config);
+                config.Dispose();
+                Console.WriteLine("[[EXITO]]");
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("[[ERROR]]", e.Message);
+            }
+
+            Console.ReadKey();
+        }
+
+   
+    }
+}
---
+++ ConsoleApplication2/confmail.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class confmail
+    {
+
+        public MailMessage definir_correo(String de, String para, String asunto, String cuerpo)
+        {
+
+            //Configuraremos el correo.
+            MailMessage correo = new MailMessage();
+            correo.To.Add(new MailAddress(de));
+            correo.From = (new MailAddress(para));
+            correo.Subject = asunto;
+            correo.Body = cuerpo;
+            correo.IsBodyHtml = true;
+            correo.Priority = MailPriority.Normal;
+
+            return correo;
+
+        }
+
+
+        public SmtpClient smtpConfig(int puerto, String host, String correo, String password)
+        {
+            SmtpClient confmail = new SmtpClient();
+            confmail.Host = host;
+            confmail.Port = puerto;
+            confmail.EnableSsl = false;
+            confmail.UseDefaultCredentials = false;
+            confmail.Credentials = new System.Net.NetworkCredential(correo, password);
+
+            return confmail;
+
+
+        }
+    }
+}
